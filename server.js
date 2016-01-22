bbf8d519b28f005c16ada60c7a9dc5b507797289
pradeep_mr <pradeep.mr@games24x7.com>
2016-01-22 15:27:31 +0530

    ip and port updated

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var receiveOrder = require('./routes/receiveOrder');
 
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = 3000
+var server_ip_address = '0.0.0.0'
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
